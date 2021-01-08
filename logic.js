563cbf0fddc9d3c55def866100af121da2bf2b39
Richard Gabaree <richard.gabaree@gmail.com>
2021-01-08 11:14:02 -0600

    added on.click events for number and operator buttons

--- logic.js
+++ logic.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
 
-    // Your code here...
-    
-    
+    var numberButton = $(".number");
+
+    numberButton.on("click", function(event){
+        event.preventDefault();
+        console.log(event);
+    })
+
+    var operatorButton = $(".operator");
+
+    operatorButton.on("click", function(event){
+        event.preventDefault();
+        console.log(event);
+    }
+    )
 });
+
