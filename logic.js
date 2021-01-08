740559740eb3e34eddbedb586db6e7b6cf6e9d06
Daniel <daniel.castelline@gmail.com>
2021-01-08 11:11:53 -0600

    js, button click functions set

--- logic.js
+++ logic.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
 
     // Your code here...
-    
-    
+    // Setting button click
+    var numberButton = $(".number");
+
+    numberButton.on("click", function(event){
+        event.preventDefault;
+        console.log(event);
+    })
+    var operatorButton = $(".operator");
+
+    operatorButton.on("click", function(event){
+        event.preventDefault;
+        console.log(event);
+    })
 });
