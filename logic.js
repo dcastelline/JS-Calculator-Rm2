6f632ac828680282695ba6f1ef3d466bd6e1b164
Daniel <daniel.castelline@gmail.com>
2021-01-08 13:08:16 -0600

    committing work

--- logic.js
+++ logic.js
@@ -1,36 +1,38 @@
 $(document).ready(function() {
 
-<<<<<<< HEAD
-    // Your code here...
-    // Setting button click
     var numberButton = $(".number");
-
-    numberButton.on("click", function(event){
-        event.preventDefault;
-        console.log(event);
-    })
     var operatorButton = $(".operator");
+    var firstNum = $("#first-number");
+    // var operator = $("#operator");
+    var secondNum = $("second-number");
+    var resultDisplay = $("#result");
+    var clear = $(".clear");
+    var equal = $(".equal");
 
-    operatorButton.on("click", function(event){
-        event.preventDefault;
-        console.log(event);
-    })
-});
-=======
-    var numberButton = $(".number");
 
     numberButton.on("click", function(event){
         event.preventDefault();
+        var num = this.value
+        console.log(this.value);
         console.log(event);
     })
 
-    var operatorButton = $(".operator");
-
     operatorButton.on("click", function(event){
         event.preventDefault();
+        var sign = this.value;
+        console.log(this.value);
         console.log(event);
+        
+    })
+    
+    function displayNum() {
+        resultDisplay.text(numberButton.value);
     }
-    )
-});
+    clear.on("click", function(event){
+        event.preventDefault();
+        resultDisplay.empty();
+        firstNum = "";
+    });
 
->>>>>>> main
+
+});
